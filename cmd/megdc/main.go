79130474002ec1a26fbebf3dc4c117dd64f6247b
ranjithamca <ranjithar@megam.io>
2016-04-06 19:49:16 +0530

    remove fmt

--- cmd/megdc/main.go
+++ cmd/megdc/main.go
@@ -16,7 +16,6 @@
 package main
 
 import (
-        "fmt"
 	"os"
 	log "github.com/Sirupsen/logrus"
 	"github.com/megamsys/megdc/packages/megam"
@@ -78,8 +77,6 @@ func cmdRegistry(name string) *cmd.Manager {
 //Run the commands from cli.
 func main() {
 	name := cmd.ExtractProgramName(os.Args[0])
-      fmt.Println("******************")
-      fmt.Println(name)
 	manager := cmdRegistry(name)
 	manager.Run(os.Args[1:])
 }
